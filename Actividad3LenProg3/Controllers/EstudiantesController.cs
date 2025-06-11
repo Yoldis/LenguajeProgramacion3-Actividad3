8ece22692c83662aff49e57835fd968530a9202e
Yoldis Rodriguez <yoldisrodriguezherrera@gmail.com>
2025-06-10 23:53:45 -0400

    Formulario, Validacion, Registrar, Listar

--- Actividad3LenProg3/Controllers/EstudiantesController.cs
+++ Actividad3LenProg3/Controllers/EstudiantesController.cs
@@ -1,22 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Actividad3LenProg3.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Actividad3LenProg3.Controllers
 {
     public class EstudiantesController : Controller
     {
+        public List<EstudianteModel> estudiantes = new List<EstudianteModel>()
+        {
+            new EstudianteModel {
+                NombreCompleto = "Juan Perez", Matricula = "2023001",
+                Carrera = "Ingeniería de Sistemas",
+                CorreoElectronico = "juan@example.com",
+                Telefono = "1234567890", FechaNacimiento = new DateTime(2000, 1, 1),
+                Genero= "Masculino", Turno = "Matutino",
+                EstaBecado = true, PorcentajeBeca = 50,
+                TipoIngreso = "Regular", TerminoCondicione = true
+               }
+        };
+        
         public IActionResult Index()
         {
-            return View();
+            return View("Formulario");
         }
-        
-        public IActionResult Registrar()
+
+        [HttpPost]
+        public IActionResult Registrar(EstudianteModel estudiante)
         {
-            return View();
+            bool existEstudiante = estudiantes.Any(e => e.Matricula == estudiante.Matricula);
+            if (!ModelState.IsValid || existEstudiante)
+            {
+                return View("formulario", estudiante);
+            }
+            estudiantes.Add(estudiante);
+            return View("Lista", estudiantes);
         }
 
         public IActionResult Lista()
         {
-            return View();
+            return View(estudiantes);
         }
 
         public IActionResult Editar()
--- Actividad3LenProg3/Models/EstudianteModel.cs
+++ Actividad3LenProg3/Models/EstudianteModel.cs
@@ -6,32 +6,40 @@ namespace Actividad3LenProg3.Models
     {
         [Required(ErrorMessage ="El nombre es requerido")]
         [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        [Display(Name = "Nombre Completo")]
         public string NombreCompleto { get; set; }
         [Required(ErrorMessage = "La matrícula es requerida")]
-        [StringLength(15, MinimumLength =6, ErrorMessage = "La matrícula debe tener minimo 6 caracteres")]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "La matrícula debe tener minimo 6 caracteres")]
         public string Matricula { get; set; }
         [Required(ErrorMessage = "La carrera es requerida")]
         public string Carrera { get; set; }
         [Required(ErrorMessage = "El correo electrónico es requerido")]
         [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        [Display(Name = "Correo Electrónico")]
         public string CorreoElectronico { get; set; }
         [Phone(ErrorMessage = "El número de teléfono no es válido")]
         [MinLength(10, ErrorMessage = "El número de teléfono debe tener al menos 10 dígitos")]
         public string Telefono { get; set; }
         [Required(ErrorMessage = "La fecha de nacimiento es requerida")]
         [DataType(DataType.Date, ErrorMessage = "La fecha de nacimiento no es válida")]
+        [Display(Name = "Fecha Nacimiento")]
         public DateTime FechaNacimiento { get; set; }
         [Required(ErrorMessage = "El género es requerido")]
         public string Genero { get; set; }
         [Required(ErrorMessage = "El turno es requerido")]
+        [Display(Name = "Turno de Estudio")]
         public string Turno { get; set; }
         [Required(ErrorMessage = "El tipo de ingreso es requerido")]
+        [Display(Name = "Tipo de Ingreso")]
         public string TipoIngreso { get; set; }
-        [Range(typeof(bool), "true", "false", ErrorMessage = "Debe seleccionar si esta becado")]
+        [Required(ErrorMessage = "El estabecado es requerido")]
+        [Display(Name = "¿Está Becado?")]
         public bool EstaBecado { get; set; }
         [Range(0, 100, ErrorMessage = "El porcentaje de beca debe estar entre 0 y 100")]
+        [Display(Name = "Porcentaje de Beca")]
         public int PorcentajeBeca { get; set; }
-        [Range(typeof(bool), "true", "false", ErrorMessage = "Debe aceptar los términos y condiciones")]
-        public bool TerminoCondiciones { get; set; }
+        [Required(ErrorMessage = "Los terminos y condicioens son requeridos")]
+        [Display(Name = "Acepto los Términos y Condiciones")]
+        public bool TerminoCondicione { get; set; }
     }
 }
